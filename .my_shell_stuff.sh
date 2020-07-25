fffde4f9bc1ce65083327fcecba3047693d254e2
binjip978 <binjip978@gmail.com>
2020-07-25 10:38:30 +0300

    stuff as well

--- .my_shell_stuff.sh
+++ .my_shell_stuff.sh
@@ -2,6 +2,7 @@ export EDITOR=vim
 
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
+export GOHUB=$GOPATH/src/github.com/binjip978
 export PATH=$PATH:$HOME/.local/bin:$GOPATH/bin
 export PATH=$PATH:/usr/local/go/bin:~/go/bin
 export PATH=$PATH:$HOME/.cargo/bin
